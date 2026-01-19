99ae53e609fde902afa6b00cebe5e74d8663d773
Saqibaqeel <Saquibaqeel@gmail.com>
2026-01-20 00:54:47 +0530

    added based url

--- frontend/src/utility/axios.js
+++ frontend/src/utility/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api', 
+    baseURL: 'https://taskmanage-qooe.onrender.com', 
     withCredentials: true, 
 });
 export default axiosInstance
